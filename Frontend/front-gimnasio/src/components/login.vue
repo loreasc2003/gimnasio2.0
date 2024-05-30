43e202c52406d858e4520bbc664075cd4b721e6e
Lorena <lorenaascencion2003@gmail.com>
2024-05-30 11:56:33 -0600

    actualizaciones

--- Frontend/front-gimnasio/src/components/login.vue
+++ Frontend/front-gimnasio/src/components/login.vue
@@ -64,7 +64,8 @@
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                             type="password" placeholder="Contraseña" />
-                        <button
+                        <a href="/register">
+                            <button
                             class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                             <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
                                 stroke-linecap="round" stroke-linejoin="round">
@@ -76,6 +77,7 @@
                                 Registrar usuario
                             </span>
                         </button>
+                        </a>
                         <p class="mt-6 text-xs text-gray-600 text-center">
                             Acepto los acuerdos de Bull´sGym
                             <a href="#" class="border-b border-gray-500 border-dotted">
--- Frontend/front-gimnasio/src/components/registerUser.vue
+++ Frontend/front-gimnasio/src/components/registerUser.vue
@@ -1,76 +1,95 @@
 <template>
-<!-- source:https://codepen.io/owaiswiz/pen/jOPvEPB -->
-<div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+    <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
     <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-            <div>
-                <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/gym-fitness-center-logo-design-template-244f8932258d15fc3f3d1e3c03cd7e2e_screen.jpg?ts=1683887714"
-                    class="w-32 mx-auto" />
-            </div>
             <div class="mt-12 flex flex-col items-center">
                 <h1 class="text-2xl xl:text-3xl font-extrabold">
-                    Registrarse
+                    Sign up
                 </h1>
-                <div class="w-full flex-1 mt-8">
-                
-                    <div class="my-12 border-b text-center">
-                        <div
-                            class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                            Datos personales
-                        </div>
-                    </div>
-
-                    <div class="mx-auto max-w-xs">
+                <div class="w-full flex-1 mt-8 grid-cols-2">
+                    <div class="grid grid-rows-2 grid-cols-2 gap-1">
                         <input
-                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                            type="email" placeholder="Nombre" />
-                        
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 col-span-2"
+                            type="text" placeholder="Nombre Completo" 
+                        />
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                            type="password" placeholder="Apellidos" />
+                            type="text" placeholder="Primer Apellido" 
+                        />
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                            type="password" placeholder="Correo electrónico" />
+                            type="text" placeholder="Segundo Apellido" 
+                        />
+                        <select id="countries" class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5">
+                            <option selected>Genero</option>
+                            <option value="M">Masculino</option>
+                            <option value="F">Femenino</option>
+                            <option value="NB">No Binario</option>
+                        </select>
+                        <select id="countries" class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5">
+                            <option selected>Tipo de Sangre</option>
+                            <option value="A+">A+</option>
+                            <option value="A-">A-</option>
+                            <option value="B+">B+</option>
+                            <option value="B-">B-</option>
+                            <option value="AB+">AB+</option>
+                            <option value="AB-">AB-</option>
+                            <option value="O+">O+</option>
+                            <option value="O-">O-</option>
+                        </select>
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 col-span-2"
+                            type="email" placeholder="Email" 
+                        />
+                        <input
+                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 "
+                            type="password" placeholder="Contraseña" 
+                        />
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                            type="password" placeholder="Contraseña" />
+                            type="date" placeholder="Fecha de Nacimiento" 
+                        />
+                  
+                        <label for="" class="font-medium col-span-2 ">Fotografia</label>
                         <input
-                            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                            type="password" placeholder="Repetir Contraseña" />
-
-
-
-                        <button
-
+                            class="w-full px-1 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-1 col-span-2"
+                            type="file" placeholder="Fotografia"
+                        />
+                        <a href="/" class="" >
+                            <button
+                                class="mt-5 tracking-wide font-semibold bg-green-700 text-gray-100 w-full py-3 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none col-span-2">
+                                <span class="ml-3 ">
+                                    Registrarse
+                                </span>
+                            </button>
                             
-                            class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                            <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                <circle cx="8.5" cy="7" r="4" />
-                                <path d="M20 8v6M23 11h-6" />
-                            </svg>
-                            <span class="ml-3">
-                                Registrarse
-                            </span>
-                        </button>
-                        <p class="mt-6 text-xs text-gray-600 text-center">
-                            Acepto los acuerdos de Bull´sGym
+                        </a>
+                        <a href="/">
+                            <button
+                                class="mt-5 tracking-wide font-semibold bg-red-700 text-gray-100 w-full py-3 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none col-span-2">
+                                <span class="ml-3 ">
+                                    Iniciar Sesión
+                                </span>
+                            </button>
+                        </a>
+                        <p class="mt-6 text-xs text-gray-600 text-center col-span-2">
+                            I agree to abide by templatana's
                             <a href="#" class="border-b border-gray-500 border-dotted">
-                                Terminos de servicio
+                                Terms of Service
                             </a>
-                            y sus
+                            and its
                             <a href="#" class="border-b border-gray-500 border-dotted">
-                                Políticas de privacidad
-                            </a>  
+                                Privacy Policy
+                            </a>
                         </p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
-            <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                style="background-image: url('https://img.freepik.com/foto-gratis/mancuernas-suelo-gimnasio-ai-generativo_123827-23744.jpg');">
+        <div class="flex-1 bg-purple-200 text-center hidden lg:flex ">
+            <div class="m-12 xl:m-16 w-10/12 bg-contain bg-center bg-no-repeat "
+                style="background-image: url('https://img.freepik.com/fotos-premium/gimnasio-muchas-maquinas-pesas-suelo_173770-1295.jpg');"
+                >
             </div>
         </div>
     </div>
