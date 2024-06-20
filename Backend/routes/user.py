a2e768c1b71fcccabcc0989e23b5593597a29da7
Lorena <lorenaascencion2003@gmail.com>
2024-06-20 11:32:34 -0600

    Creación de rutas get y post para users

--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -1,8 +1,32 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
 
 user = APIRouter()
+users = []
+
+#userModel esto iria en MODELS.py
+class model_user(BaseModel):
+        id:str
+        usuario:str
+        password: str
+        created_at:datetime = datetime.now
+        estatus:bool=False 
+
+@user.get("/")
+
+def bienvenida():
+        return "Bienvenido al sistema de APIs"
 
 @user.get("/users")
 
-def helloworld():
-        return "PORQUE ERES MI NOVIO CRISTIAAAAAN!"
+def get_usuarios():
+        return users
+
+
+@user.post('/users')
+
+def save_usuarios(users:model_user):
+        #users.append
+        print (users)
+        return "Datos guardados"
