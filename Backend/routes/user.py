a3e180299beb4f341a2a1b4dfc4060eaa63c4af9
Lorena <210885@utxicotepec.edu.mx>
2024-06-24 20:22:01 -0600

    tarea: actualizar, eliminar, buscar

--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -30,3 +30,26 @@ def save_usuarios(insert_users:model_user):
         users.append(insert_users)
         #print (users)
         return "Datos guardados"
+
+@user.get('/users/')
+def get_usuario(id: str):
+    for user in users:
+        if user.id == id:
+            return user
+    return {"error": "Usuario no encontrado"}
+
+@user.put('/users')
+def update_usuario(user_id: str, updated_user: model_user):
+    for idx, user in enumerate(users):
+        if user.id == id:
+            users[idx] = updated_user
+            return "Datos actualizados"
+    return {"error": "Usuario no encontrado"}
+
+@user.delete('/users')
+def delete_usuario(id: str):
+    for idx, user in enumerate(users):
+        if user.id == id:
+            del users[idx]
+            return "Usuario eliminado"
+    return {"error": "Usuario no encontrado"}
