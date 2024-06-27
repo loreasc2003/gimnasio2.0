b78b1025d7a2752f29dccd7ef893faadbc1b4978
Lorena <lorenaascencion2003@gmail.com>
2024-06-27 11:46:25 -0600

    Creación de archivos de crud y schemas

--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -1,58 +1,62 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
-from datetime import datetime
+from fastapi import APIRouter,Depends
+import schemas, models
+from cruds import crud
+from sqlalchemy.orm import Session
+from config.db import SessionLocal, engine
+
+import schemas
 
 user = APIRouter()
-users = []
 
-#userModel esto iria en MODELS.py
-class model_user(BaseModel):
-        id:str
-        usuario:str
-        password: str
-        created_at:datetime = datetime.now()
-        estatus:bool=False 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @user.get("/", tags=['Bienvenida'])
 
 def bienvenida():
         return "Bienvenido al sistema de APIs"
 
-@user.get("/users", tags=['Usuarios'])
+@user.get("/users",response_model=list[schemas.users], tags=['Usuarios'])
 
-def get_usuarios():
+def get_usuarios(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+        users = crud.get_users(db, skip=skip, limit=limit)
         return users
 
 
-@user.post('/users', tags=['Usuarios'])
+# @user.post('/users', tags=['Usuarios'])
 
-def save_usuarios(insert_users:model_user):
-        users.append(insert_users)
-        #print (users)
-        return "Datos guardados"
+# def save_usuarios(insert_users:model_user):
+#         users.append(insert_users)
+#         #print (users)
+#         return "Datos guardados"
 
-# BUSCAR
-@user.post('/users/', tags=['Usuarios'])
-def post_usuario(id: str):
-    for user in users:
-        if user.id == id:
-            return user
-    return {"error": "Usuario no encontrado"}
+# # BUSCAR
+# @user.post('/users/', tags=['Usuarios'])
+# def post_usuario(id: str):
+#     for user in users:
+#         if user.id == id:
+#             return user
+#     return {"error": "Usuario no encontrado"}
 
-#ACTUALIZAR
-@user.put('/users', tags=['Usuarios'])
-def update_usuario(id: str, updated_user: model_user):
-    for idx, user in enumerate(users):
-        if user.id == id:
-            users[idx] = updated_user
-            return "Datos actualizados"
-    return {"error": "Usuario no encontrado"}
+# #ACTUALIZAR
+# @user.put('/users', tags=['Usuarios'])
+# def update_usuario(id: str, updated_user: model_user):
+#     for idx, user in enumerate(users):
+#         if user.id == id:
+#             users[idx] = updated_user
+#             return "Datos actualizados"
+#     return {"error": "Usuario no encontrado"}
 
-# ELIMINAR
-@user.delete('/users', tags=['Usuarios'])
-def delete_usuario(id: str):
-    for idx, user in enumerate(users):
-        if user.id == id:
-            del users[idx]
-            return "Usuario eliminado"
-    return {"error": "Usuario no encontrado"}
+# # ELIMINAR
+# @user.delete('/users', tags=['Usuarios'])
+# def delete_usuario(id: str):
+#     for idx, user in enumerate(users):
+#         if user.id == id:
+#             del users[idx]
+#             return "Usuario eliminado"
+#     return {"error": "Usuario no encontrado"}
---
+++ Backend/schemas/users.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    usuario: str
+    password: str
+    estatus: bool=False
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
